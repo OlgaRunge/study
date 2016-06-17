9ed8aaf77abdb0c160e3eb301c89d25d91d6ad53
OlgaRunge <angelicaiwf@mail.ru>
2016-06-17 13:36:46 +0300

    last change (reverted from commit d75fb559aec10f32bd41afc1d5cca20ff96d3bb2)

--- index.php
+++ index.php
@@ -15,36 +15,3 @@ if ($a>0) {
 } else {
  echo 'a не больше 0';
 }
-?>
-<br>
-<br>
-
-<?php
-$a = 5;
-if ($a>0) {
- echo 'a больше 0';
-} else {
- if ($a < 0) {
-  echo 'a не больше 0';
- } else {
-  echo 'a равно 0';
- }
-}
-?>
-
-<br>
-
-<?php
-$a = 5;
-if ($a > 0) {
- echo 'a больше 0';
-} else if ($a < 0){
-  echo 'a меньше 0';
-} else {
- echo 'a равно 0';
-}
-7>
-
-<br>
-
-echo "hi";
